637124883edb1ee4cb7db1eaa533786d038d1f62
Natan Szymczak <natan.szymczak@gmail.com>
2020-10-08 23:34:41 +0200

    Connect to database

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 app.get('/', (req, res) => {
   res.send('HOME');
 });
+
+// Connect to database
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Connected to database.')
+);
+
 
 app. listen(3000);
